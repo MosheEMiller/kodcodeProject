93a7cdc1ed7559f61348ae95bdf8358c1bcc5088
MosheEMiller <0548470675m@gmail.com>
2024-06-18 13:28:26 +0300

    add-flags-file

---
+++ client/flags.js
@@ -0,0 +1,17 @@
+const specializationsFlags = {
+    "psychology": "Psychology",
+    "psychiatry": "Psychiatry",
+    "psychotherapy": "Psychotherapy",
+    "psychoanalysis": "Psychoanalysis",
+    "psychotherapy": "Psychotherapy",
+}
+
+const areasFlags = {
+    north: "North",
+    south: "South",
+    east: "East",
+    west: "West",
+    central: "Central",
+}
+
+module.exports = { specializationsFlags, areasFlags };
